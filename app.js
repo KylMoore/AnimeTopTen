7dda3252e6e6297220aa223d4fe53d838d6e08e9
Jimmy Kang <sjimmy.kang@gmail.com>
2023-01-06 11:36:10 -0500

    feature url finalized

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const url = new URL('https://api.jikan.moe/v4/anime?start_date=2022&rating=g');
+const url = new URL('https://api.jikan.moe/v4/anime');
 /* to make query */
 
-
+/* limit search param to be anime with g rating with descending highest score to grab top 10 */
+url.search = new URLSearchParams({
+    rating: 'g',
+    start_date: 2022,
+    // page: 1,
+    limit: 10,
+    order_by: 'score',
+    sort: 'desc'
+})
 
 fetch(url)
-    // .then(res => res.json())
     .then(res => {
         // console.log(res)
         return res.json();
