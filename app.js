8811939d7d3dfb12c5e73928ae3ef8ad92da7bd2
Jimmy Kang <sjimmy.kang@gmail.com>
2023-01-16 02:07:05 -0500

    adjusted nav select width and cleaned up spacings

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
 /* namespacing */
 const animeTopTen = {};
 animeTopTen.filtered = [];
-
 /* base api url */
 animeTopTen.url = 'https://api.jikan.moe/v4/anime';
 
 animeTopTen.init = () => {
     animeTopTen.displayAll();
-
     animeTopTen.setUpEventListeners();
-
     animeTopTen.pageLoad();
 };
 
-//to reset our selection to default on pageload
+/* to reset our selections to default on pageload */
 animeTopTen.pageLoad = () => {
     const yearEl = document.querySelector("#year");
     const genreEl = document.querySelector("#genre");
-
     yearEl[0].selected = true;
     genreEl[0].selected = true;
 }
 
-animeTopTen.displayAll = (year = "time-2021-Present", selectGenre = "") => {
-
-    let yearStart = "";
-    let yearEnd = 2023;
-
-
+animeTopTen.displayAll = (year = "all-Time", selectGenre = "") => {
+    let yearStart = 1995;
+    //get current year: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
+    const currentDate = new Date();
+    let yearEnd = currentDate.getFullYear();
 
     if (year === 'time-2021-Present') {
         yearStart = 2021;
-        yearEnd = 2023;
-    } else if (year === 'all-Time') {
-        yearStart = 1995;
-        yearEnd = 2023;
     } else if (year === 'time-2001-2010') {
         yearStart = 2001;
         yearEnd = 2010;
@@ -45,9 +36,7 @@ animeTopTen.displayAll = (year = "time-2021-Present", selectGenre = "") => {
 
     /* to make query */
     const url = new URL(animeTopTen.url);
-
-
-    /* limit search param to be anime with g rating with descending highest score to grab top 10 */
+    /* limit search param to be anime with g rating with descending highest score to grab top 20 */
     url.search = new URLSearchParams({
         rating: 'pg13',
         start_date: yearStart,
@@ -56,41 +45,31 @@ animeTopTen.displayAll = (year = "time-2021-Present", selectGenre = "") => {
         limit: 25,
         genres: selectGenre,
         order_by: 'score',
-        // order_by: 'members',
-        sort: 'desc',
-        // type: 'tv'
-
-        
+        sort: 'desc'
     })
-    
     
     fetch(url)
         .then(res => {
             return res.json();
         })
         .then(jsonResult => {
-
             document.querySelector(".animeGallery").innerHTML = "";
-
             animeTopTen.rankTen = jsonResult.data;
-
-
+            // empty out the .filtered array to fill it again
             animeTopTen.filtered = [];
+            /* to filter out edge cases with high score but very little views and loop to fill 10 slots */
             animeTopTen.rankTen.forEach((anime) => {
                 if (anime.members > 10000 && animeTopTen.filtered.length < 10) {
                     animeTopTen.filtered.push(anime)
                 }
             })
-
             animeTopTen.displayAnime();
         })
 };
 
 animeTopTen.displayAnime = () => {
-
     const ulEl = document.querySelector('.animeGallery');
     animeTopTen.filtered.forEach((anime) => {
-
         const liEl = document.createElement('li');
         const aEl = document.createElement('a');
         const titleEl = document.createElement('h3');
@@ -104,7 +83,7 @@ animeTopTen.displayAnime = () => {
         scoreEl.textContent = anime.score;
         imgEl.src = anime.images.jpg.image_url;
         imgEl.alt = `poster picture for ${anime.title}`;
-        linkEl.textContent = `Click image learn more`
+        linkEl.textContent = `Click to go MyAnimeList for more detail`
         themeEl.classList.add('text')
 
         let themeTags = '';
@@ -124,29 +103,19 @@ animeTopTen.displayAnime = () => {
         aEl.appendChild(imgEl);
         aEl.appendChild(linkEl);
         liEl.appendChild(themeEl);
-
-
-
         ulEl.appendChild(liEl)
     });
 };
-
-
 
 animeTopTen.setUpEventListeners = function () {
     document.querySelector("#search").addEventListener("click", function (e) {
         e.preventDefault();
         const year = document.querySelector("#year");
         const genre = document.querySelector("#genre");
-
         const selectedYear = year.selectedOptions[0].value;
         const selectedGenre = genre.selectedOptions[0].value;
-
-
         animeTopTen.displayAll(selectedYear, selectedGenre);
     });
 }
-
-
 
 animeTopTen.init();
